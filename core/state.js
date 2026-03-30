8a6b609ce8a8507e9cbe4ebef4790e3fe098d214
yeipromero-goat <yeipromero@gmail.com>
2026-03-29 20:44:03 -0600

    Create state.js
    test 1

---
+++ core/state.js
@@ -0,0 +1,17 @@
+export const state = {
+  game: {
+    score: 0
+  },
+
+  bow: {
+    name: "GOAT Alpha",
+    level: 1,
+    points: 5,
+
+    stats: {
+      fps: 320,
+      letOff: 80,
+      braceHeight: 6
+    }
+  }
+};
